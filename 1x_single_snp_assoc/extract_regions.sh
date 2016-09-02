25333616ef7a167dc05e76e2d925837b67738728
vincentrose88 <vincentrose88@gmail.com>
2016-09-02 11:08:01 +0100

    bug squashing

--- 1x_single_snp_assoc/extract_regions.sh
+++ 1x_single_snp_assoc/extract_regions.sh
@@ -7,5 +7,5 @@ do
     min=`cut -f2 $i | sort -n | head -1`
     max=`cut -f3 $i | sort -n | tail -1`
     echo -e "gene:\t$i\nChr:\t$chr\nstart\t$min\nend\t$max"
-    zcat $(dirname $0)/MANOLIS.$phenotype.assoc.txt.gz | awk -v "min=$min" -v "max=$max" -v "chr=$chr" 'NR==1 || ($1==chr && $3<=max && $3>min)' > $1/output/$gene.assoc
+    zcat $(dirname $0)/MANOLIS.$phenotype.assoc.txt.gz | awk -v "min=$min" -v "max=$max" -v "chr=$chr" 'NR==1 || ($1==chr && $3<=max && $3>min)' > $1/$2/output/$gene.assoc
 done
