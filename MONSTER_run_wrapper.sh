994fdcad6eb34d7eab79b144712044c67148c009
vincentrose88 <vincentrose88@gmail.com>
2016-09-06 10:11:03 +0100

    tried to fix MONSTER_run to run parallallized

--- MONSTER_run_wrapper.sh
+++ MONSTER_run_wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash /nfs/team144/ds26/burden_testing/scripts/MONSTER_wrapper.sh $1/$2/$1.$2_genotype $1/$2/$1.$2_variants 15x_transformed_phenos_4testing/manolis_$1.txt $1/$2/output/$1.$2.monster
+cd $1/$2 && bash /nfs/team144/ds26/burden_testing/scripts/MONSTER_wrapper.sh $1.$2_genotype $1.$2_variants 15x_transformed_phenos_4testing/manolis_$1.txt output/$1.$2.monster
 rm *mod*
