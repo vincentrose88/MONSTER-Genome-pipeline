d663517dda453082b5529db60986dd6c15dc61fe
vincentrose88 <vincentrose88@gmail.com>
2016-09-09 16:04:10 +0100

    debuggin new setup

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ for i in $traits;
 do 
     mkdir $i/
     cat $inputfile | awk -v "trait=$i" '$2==trait {print $1}'| sort -u > $i/$i.genes
+    echo -e "$i firstRun -g exon -MAF 0.05 -s Eigen -b 1" >> firstRun.parameters
 done
-echo -e "$traits firstRun -g exon -MAF 0.05 -s Eigen -b 1" | tr ' ' '\n' > firstRun.parameters
+
 chmod +x *.sh
 chmod +x *.py
 chmod +x *.R
