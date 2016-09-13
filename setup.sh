1991029cc58b4437642e16b921b4ae33c454a4df
vincentrose88 <vincentrose88@gmail.com>
2016-09-13 10:48:31 +0100

    debugging

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ inputTraitGene=`cat $inputfile | awk '$2!="Trait" || '$1!="Gene" {print $0}' | s
 for i in $inputTraitGene;
     do 
     gene=`awk '{print $1}'`
-    trait`awk '{print $2}'`
+    trait=`awk '{print $2}'`
     mkdir -p $trait/$gene
     echo -e "$gene" > $trait/$gene/$trait.$gene
     echo -e "$trait $gene exon.MAF0.05.EigenWeights.b1 -g exon -MAF 0.05 -s Eigen -b 1" >> firstRun.parameters
