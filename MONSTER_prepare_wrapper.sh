0378f04cc75aa1a8c4547e8138d714e68f019b02
vincentrose88 <vincentrose88@gmail.com>
2016-09-13 09:58:28 +0100

    Changed to run each gene separately and merged the prepare and run script into one

--- MONSTER_prepare_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pheno=$1; shift
-outputSuffix=$1; shift
-otherArguments=$@
-mkdir -p $pheno/$outputSuffix/output/plots
-
-cd $pheno/$outputSuffix && perl /nfs/team144/ds26/burden_testing/scripts/burden_get_regions_v2.5.pl -i ../$pheno.genes -o $pheno.$outputSuffix -v $otherArguments
--- setup.sh
+++ setup.sh
@@ -2,11 +2,15 @@
 inputfile=$1
 
 traits=`cat $inputfile | awk '$2!="Trait" {print $2}' | sort -u`
+genes=`cat $inputfile | awk '$1!="Gene" {print $1}' | sort -u`
 for i in $traits; 
 do 
-    mkdir $i/
-    cat $inputfile | awk -v "trait=$i" '$2==trait {print $1}'| sort -u > $i/$i.genes
-    echo -e "$i firstRun.exon.MAF0.05.EigenWeights.b1 -g exon -MAF 0.05 -s Eigen -b 1" >> firstRun.parameters
+    for j in $genes;
+    do 
+	mkdir -p $i/$j
+	cat $inputfile | awk -v "trait=$i" -v "gene=$k" '$2==trait && $1==gene {print $1}' | sort -u > $i/$j/$i.$j
+	echo -e "$i $j exon.MAF0.05.EigenWeights.b1 -g exon -MAF 0.05 -s Eigen -b 1" >> firstRun.parameters
+    done
 done
 
 chmod +x *.sh
