e92e96e15f7e0436a2a11e2e98bfe9d5ff33147a
vincentrose88 <vincentrose88@gmail.com>
2016-09-13 13:35:54 +0100

    debuggin

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 inputfile=$1
 
-inputTraitGene=`cat $inputfile | awk '$2!="Trait" || $1!="Gene" {print $0}'`
-for i in $inputTraitGene;
+while read i; 
     do 
-    gene=`awk '{print $1}' <($i)`
-    trait=`awk '{print $2}' <($i)`
+    gene=`awk '{print $1}' <(echo $i)`
+    trait=`awk '{print $2}' <(echo $i)`
     mkdir -p $trait/$gene
     echo -e "$gene" > $trait/$gene/$trait.$gene
     echo -e "$trait $gene exon.MAF0.05.EigenWeights.b1 -g exon -MAF 0.05 -s Eigen -b 1" >> firstRun.parameters
-done
+done < $inputfile
 
 chmod +x *.sh
 chmod +x *.py
