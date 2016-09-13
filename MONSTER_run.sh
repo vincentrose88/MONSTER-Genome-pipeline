636c4105f4a92c0f5741884988bdc7c5a10c84cc
vincentrose88 <vincentrose88@gmail.com>
2016-09-13 14:19:44 +0100

    debugging

--- MONSTER_run.sh
+++ MONSTER_run.sh
@@ -4,4 +4,4 @@ gene=$1; shift
 outputSuffix=$1; shift
 otherArguments=$@
 
-cd $pheno/$gene/$outputSuffix && bash /nfs/team144/ds26/burden_testing/scripts/MONSTER_wrapper.sh $pheno.${geno}_genotype $pheno.${geno}_variants ../../../15x_transformed_phenos/${pheno}_HA.txt output/$pheno.$geno.monster
+cd $pheno/$gene/$outputSuffix && bash /nfs/team144/ds26/burden_testing/scripts/MONSTER_wrapper.sh $pheno.${gene}_genotype $pheno.${gene}_variants ../../../15x_transformed_phenos/${pheno}_HA.txt output/$pheno.$geno.monster
