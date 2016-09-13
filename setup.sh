489bf427ec2240085c2e8700bc057cc8d2a33425
vincentrose88 <vincentrose88@gmail.com>
2016-09-13 13:40:52 +0100

    debuggin

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ while read i;
     trait=`awk '{print $2}' <(echo $i)`
     mkdir -p $trait/$gene
     echo -e "$gene" > $trait/$gene/$trait.$gene
-    echo -e '$trait $gene exon.MAF0.05.EigenWeights.b1 -g exon -MAF 0.05 -s Eigen -b 1' >> firstRun.parameters
+    echo -e "$trait $gene exon.MAF0.05.EigenWeights.b1 -g exon -MAF 0.05 -s Eigen -b 1" >> firstRun.parameters
 done < $inputfile
 
 chmod +x *.sh
