2ffc035dfd6354bc60faa28a7c73491b02a0acf3
vincentrose88 <vincentrose88@gmail.com>
2016-09-21 16:48:26 +0100

    updated to 2.53 version of prepare MONSTER source script

--- MONSTER_prepare.sh
+++ MONSTER_prepare.sh
@@ -5,4 +5,4 @@ outputSuffix=$1; shift
 otherArguments=$@
 mkdir -p $pheno/$gene/$outputSuffix/output/plots
 
-cd $pheno/$gene/$outputSuffix && perl /nfs/team144/ds26/burden_testing/scripts/burden_get_regions_v2.5.pl -i ../$pheno.$gene -o $pheno.$gene -v $otherArguments
+cd $pheno/$gene/$outputSuffix && perl /nfs/team144/ds26/burden_testing/scripts/burden_get_regions_v2.53.pl -i ../$pheno.$gene -o $pheno.$gene -v $otherArguments
