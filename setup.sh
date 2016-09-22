a209e4416152d2325ffc32318d2a47bbfa9b85b8
vincentrose88 <vincentrose88@gmail.com>
2016-09-22 11:41:11 +0100

    updated setup to new default trait gene list

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ inputfile=$1
 
 while read i; 
     do 
-    gene=`awk '{print $1}' <(echo $i)`
-    trait=`awk '{print $2}' <(echo $i)`
+    gene=`awk '{print $2}' <(echo $i)`
+    trait=`awk '{print $1}' <(echo $i)`
     mkdir -p $trait/$gene
     echo -e "$gene" > $trait/$gene/$trait.$gene
     echo -e "$trait $gene exon.MAF0.05.mSNP0.01 -G exon -MAF 0.05 -m 0.01" >> firstRun.par
